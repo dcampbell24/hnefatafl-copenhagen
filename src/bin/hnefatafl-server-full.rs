7c2be7cc690bcc88cf467463eab63736a9bf8c96
David Campbell <dcampbell24@gmail.com>
2025-01-20 00:52:20 -0500

    Remove unnecessary comments.

--- src/bin/hnefatafl-server-full.rs
+++ src/bin/hnefatafl-server-full.rs
@@ -1,13 +1,3 @@
-// You can connect to this server
-// for now the server will hand out a user number
-//
-// You can create or join a game
-// list all the available games
-// watch a game in progress
-// review already played games
-//
-// a server has a database of finished games
-
 use std::{
     collections::{HashMap, HashSet},
     io::{BufRead, BufReader},
