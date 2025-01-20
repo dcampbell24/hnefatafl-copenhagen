af23261b7d4f61387f1aa46e60ff6caa3b116e2e
David Campbell <dcampbell24@gmail.com>
2025-01-20 01:00:15 -0500

    Use a byte string literal.

--- src/bin/hnefatafl-client.rs
+++ src/bin/hnefatafl-client.rs
@@ -5,9 +5,9 @@ fn main() -> anyhow::Result<()> {
     let mut stream = TcpStream::connect(&address)?;
     println!("connected to {address} ...");
 
-    let username = "foobar";
-    stream.write_all(username.as_bytes())?;
-    stream.write_all("\n".as_bytes())?;
+    let username = b"foobar";
+    stream.write_all(username)?;
+    stream.write_all(b"\n")?;
 
     sleep(Duration::from_secs(10));
     // let mut game = Game::default();
