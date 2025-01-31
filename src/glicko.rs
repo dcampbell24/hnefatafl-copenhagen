146e20569b96d830b688990b57d481c4a5d03c78
David Campbell <dcampbell24@gmail.com>
2025-01-31 18:33:50 -0500

    Finish the basic glicko.

--- src/glicko.rs
+++ src/glicko.rs
@@ -11,6 +11,7 @@ const Q: f64 = 0.005_756_5;
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Rating {
     pub rating: f64,
+    /// Ratings Deviation
     pub rd: f64,
 }
 
@@ -29,17 +30,17 @@ impl Rating {
     }
 
     pub fn update_rating(&mut self, rating: f64, outcome: &Outcome) {
-        self.rating += (Q / (1.0 / self.rd_sq()) + (1.0 / self.d_sq()))
+        self.rating += (Q / (1.0 / self.rd_sq()) + (1.0 / self.d_sq(rating)))
             * self.g()
             * (outcome.score() - self.e(rating));
 
-        self.rd = f64::sqrt(1.0 / ((1.0 / self.rd_sq()) + (1.0 / self.d_sq())));
+        self.rd = f64::sqrt(1.0 / ((1.0 / self.rd_sq()) + (1.0 / self.d_sq(rating))));
     }
 
     // Fixme:
     #[must_use]
-    fn d_sq(&self) -> f64 {
-        1.0 + self.rating - self.rating
+    fn d_sq(&self, rating: f64) -> f64 {
+        1.0 / ((Q * Q) * (self.g() * self.g()) * self.e(rating) * (1.0 - self.e(rating)))
     }
 
     #[must_use]
@@ -94,6 +95,8 @@ pub fn exp10(mut exponent: f64) -> f64 {
 
 #[cfg(test)]
 mod tests {
+    use crate::glicko::Outcome;
+
     use super::{exp10, Rating};
 
     #[test]
@@ -116,5 +119,30 @@ mod tests {
         rating.rd = 300.0;
         rating.update_rd();
         assert_eq!(rating.rd.round(), 307.0);
+    }
+
+    #[test]
+    fn rating_and_rd_changes() {
+        let rating = Rating::default();
+        assert_eq!(rating.rating, 1_500.0);
+
+        let mut rating_1 = rating.clone();
+        rating_1.update_rating(1_600.0, &Outcome::Win);
+        assert_eq!(rating_1.rating.round(), 1_781.0);
+
+        let mut rating_2 = rating.clone();
+        rating_2.update_rating(1_500.0, &Outcome::Win);
+        assert_eq!(rating_2.rating.round(), 1_736.0);
+
+        let mut rating_3 = rating.clone();
+        rating_3.update_rating(1_500.0, &Outcome::Loss);
+        assert_eq!(rating_3.rating.round(), 1_264.0);
+
+        let mut rating_4 = rating.clone();
+        rating_4.update_rating(1_500.0, &Outcome::Loss);
+        assert_eq!(rating_4.rating.round(), 1_264.0);
+        assert_eq!(rating_4.rd.round(), 299.0);
+        rating_4.update_rd();
+        assert_eq!(rating_4.rd.round(), 305.0);
     }
 }
