52bb7d8dc2efcf05426868d4ea0419885b018cd2
David Campbell <dcampbell24@gmail.com>
2025-02-28 20:05:37 -0500

    Make it ok if permission is denied in build.rs.

--- build.rs
+++ build.rs
@@ -1,24 +1,35 @@
-use std::{fs, io::Error, path::Path};
+use std::{fs, io, path::Path};
 
 static HOME: &str = "hnefatafl-copenhagen";
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), io::Error> {
     if let Some(mut path) = dirs::data_dir() {
         path = path.join(HOME);
         fs::create_dir_all(&path)?;
 
-        fs::copy(
+        ok_permission_denied(fs::copy(
             Path::new("sound").join("capture.ogg"),
             path.join("capture.ogg"),
-        )?;
+        ))?;
 
-        fs::copy(
+        ok_permission_denied(fs::copy(
             Path::new("sound").join("game_over.ogg"),
             path.join("game_over.ogg"),
-        )?;
+        ))?;
 
-        fs::copy(Path::new("sound").join("move.ogg"), path.join("move.ogg"))?;
+        ok_permission_denied(fs::copy(
+            Path::new("sound").join("move.ogg"),
+            path.join("move.ogg"),
+        ))?;
     }
 
     Ok(())
+}
+
+fn ok_permission_denied(error: io::Result<u64>) -> io::Result<()> {
+    match error {
+        Ok(_) => Ok(()),
+        Err(error) if error.kind() == io::ErrorKind::PermissionDenied => Ok(()),
+        Err(error) => Err(error),
+    }
 }
