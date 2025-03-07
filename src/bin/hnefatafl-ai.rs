0373d8a53c148ae92224c39da8fef80d24242f1c
David Campbell <dcampbell24@gmail.com>
2025-03-07 15:46:04 -0500

    Add AI that doesn't depend on hnefatafl-rs.

---
+++ src/bin/hnefatafl-ai.rs
@@ -0,0 +1,209 @@
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::TcpStream,
+    str::FromStr,
+};
+
+use anyhow::Error;
+use clap::{Parser, command};
+use hnefatafl_copenhagen::{
+    VERSION_ID, color::Color, game::Game, play::Vertex, role::Role, status::Status,
+};
+
+// Move 26, defender wins, corner escape, time per move 15s 2025-03-06 (hnefatafl-equi).
+
+const PORT: &str = ":49152";
+
+/// A Hnefatafl Copenhagen AI
+///
+/// This is an AI client that connects to a server.
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    #[arg(long)]
+    username: String,
+
+    #[arg(default_value = "", long)]
+    password: String,
+
+    /// attacker or defender
+    #[arg(long)]
+    role: Role,
+
+    /// Connect to the HTP server at host
+    #[arg(default_value = "hnefatafl.org", long)]
+    host: String,
+
+    /// Join game with id
+    #[arg(long)]
+    join_game: Option<u64>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let mut username = "ai-".to_string();
+    username.push_str(&args.username);
+
+    let mut address = args.host.to_string();
+    address.push_str(PORT);
+
+    let mut buf = String::new();
+    let mut tcp = TcpStream::connect(address)?;
+    let mut reader = BufReader::new(tcp.try_clone()?);
+
+    tcp.write_all(format!("{VERSION_ID} login {username} {}\n", args.password).as_bytes())?;
+    reader.read_line(&mut buf)?;
+    assert_eq!(buf, "= login\n");
+    buf.clear();
+
+    loop {
+        let game_id;
+
+        if let Some(game_id_) = args.join_game {
+            game_id = game_id_.to_string();
+            tcp.write_all(format!("join_game_pending {game_id}\n").as_bytes())?;
+        } else {
+            new_game(&mut tcp, args.role, &mut reader, &mut buf)?;
+
+            let message: Vec<_> = buf.split_ascii_whitespace().collect();
+            game_id = message[3].to_string();
+            buf.clear();
+
+            wait_for_challenger(&mut reader, &mut buf, &mut tcp, &game_id)?;
+        }
+
+        let game = Game::default();
+        println!("{}", game.board);
+
+        handle_messages(game, &game_id, &mut reader, &mut tcp)?;
+
+        if args.join_game.is_some() {
+            return Ok(());
+        }
+    }
+}
+
+// "= new_game game GAME_ID ai-00 _ rated fischer 900000 10 _ false {}\n"
+fn new_game(
+    tcp: &mut TcpStream,
+    role: Role,
+    reader: &mut BufReader<TcpStream>,
+    buf: &mut String,
+) -> anyhow::Result<()> {
+    tcp.write_all(format!("new_game {role} rated fischer 900000 10\n").as_bytes())?;
+
+    loop {
+        // "= new_game game GAME_ID ai-00 _ rated fischer 900000 10 _ false {}\n"
+        reader.read_line(buf)?;
+
+        if buf.trim().is_empty() {
+            return Err(Error::msg("the TCP stream has closed"));
+        }
+
+        let message: Vec<_> = buf.split_ascii_whitespace().collect();
+        if message[1] == "new_game" {
+            return Ok(());
+        }
+
+        buf.clear();
+    }
+}
+
+fn wait_for_challenger(
+    reader: &mut BufReader<TcpStream>,
+    buf: &mut String,
+    tcp: &mut TcpStream,
+    game_id: &str,
+) -> anyhow::Result<()> {
+    loop {
+        reader.read_line(buf)?;
+
+        if buf.trim().is_empty() {
+            return Err(Error::msg("the TCP stream has closed"));
+        }
+
+        let message: Vec<_> = buf.split_ascii_whitespace().collect();
+        if Some("challenge_requested") == message.get(1).copied() {
+            println!("{message:?}");
+            buf.clear();
+
+            break;
+        }
+
+        buf.clear();
+    }
+
+    tcp.write_all(format!("join_game {game_id}\n").as_bytes())?;
+    Ok(())
+}
+
+fn handle_messages(
+    mut game: Game,
+    game_id: &str,
+    reader: &mut BufReader<TcpStream>,
+    tcp: &mut TcpStream,
+) -> anyhow::Result<()> {
+    let mut buf = String::new();
+    loop {
+        reader.read_line(&mut buf)?;
+
+        if buf.trim().is_empty() {
+            return Err(Error::msg("the TCP stream has closed"));
+        }
+
+        let message: Vec<_> = buf.split_ascii_whitespace().collect();
+
+        if Some("generate_move") == message.get(2).copied() {
+            let play = game.generate_move().expect("the game must be in progress");
+
+            println!("\n{play}");
+            game.play(&play)?;
+
+            tcp.write_all(format!("game {game_id} {play}").as_bytes())?;
+            println!("{}", game.board);
+
+            if game.status != Status::Ongoing {
+                return Ok(());
+            }
+        } else if Some("play") == message.get(2).copied() {
+            let Some(color) = message.get(3).copied() else {
+                panic!("expected color");
+            };
+            let Ok(color) = Color::from_str(color) else {
+                panic!("expected color to be a color");
+            };
+
+            let Some(from) = message.get(4).copied() else {
+                panic!("expected from");
+            };
+            if from == "resigns" {
+                return Ok(());
+            }
+            let Ok(from) = Vertex::from_str(from) else {
+                panic!("expected from to be a vertex");
+            };
+
+            let Some(to) = message.get(5).copied() else {
+                panic!("expected to");
+            };
+            let Ok(to) = Vertex::from_str(to) else {
+                panic!("expected to to be a vertex");
+            };
+
+            let play = format!("play {color} {from} {to}\n");
+            print!("{play}");
+            game.read_line(&play)?;
+
+            if game.status != Status::Ongoing {
+                return Ok(());
+            }
+
+            println!("{}", game.board);
+        } else if Some("game_over") == message.get(1).copied() {
+            return Ok(());
+        }
+
+        buf.clear();
+    }
+}
