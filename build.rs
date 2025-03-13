3319207cff62270a5413e50a0aba75b5dbd82c43
David Campbell <dcampbell24@gmail.com>
2025-03-13 00:32:55 -0400

    Don't run the buils.rs for docrs.

--- build.rs
+++ build.rs
@@ -1,35 +1,27 @@
+#[cfg(not(docsrs))]
 use std::{fs, io, path::Path};
 
+#[cfg(not(docsrs))]
 static HOME: &str = "hnefatafl-copenhagen";
 
+#[cfg(not(docsrs))]
 fn main() -> Result<(), io::Error> {
     if let Some(mut path) = dirs::data_dir() {
         path = path.join(HOME);
         fs::create_dir_all(&path)?;
 
-        ok_permission_denied(fs::copy(
+        fs::copy(
             Path::new("sound").join("capture.ogg"),
             path.join("capture.ogg"),
-        ))?;
+        )?;
 
-        ok_permission_denied(fs::copy(
+        fs::copy(
             Path::new("sound").join("game_over.ogg"),
             path.join("game_over.ogg"),
-        ))?;
+        )?;
 
-        ok_permission_denied(fs::copy(
-            Path::new("sound").join("move.ogg"),
-            path.join("move.ogg"),
-        ))?;
+        fs::copy(Path::new("sound").join("move.ogg"), path.join("move.ogg"))?;
     }
 
     Ok(())
-}
-
-fn ok_permission_denied(error: io::Result<u64>) -> io::Result<()> {
-    match error {
-        Ok(_) => Ok(()),
-        Err(error) if error.kind() == io::ErrorKind::PermissionDenied => Ok(()),
-        Err(error) => Err(error),
-    }
 }
