91b5dc709e1c73d0746471197d06b87305e65d5b
David Campbell <dcampbell24@gmail.com>
2025-04-15 18:09:15 -0400

    Update the website.

--- packages/nsis/build.ps1
+++ packages/nsis/build.ps1
@@ -6,5 +6,6 @@
 # https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe
 # Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass
 
+git checkout v0.13.2
 cargo build --bin hnefatafl-client --features client --release
 & 'C:\Program Files (x86)\NSIS\Bin\makensis.exe' .\hnefatafl-client-installer.nsi
