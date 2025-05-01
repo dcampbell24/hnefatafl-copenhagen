ddb47f4ce9316826070469b1b8369c04fd3beb43
David Campbell <dcampbell24@gmail.com>
2025-05-01 01:05:52 -0400

    Add an AI manpage.

--- packages/deb.sh
+++ packages/deb.sh
@@ -1,6 +1,15 @@
 #! /bin/bash -e
 
 # https://github.com/jgm/pandoc/releases/latest
+
+pandoc\
+    --variable=title:hnefatafl-ai\
+    --variable=section:1\
+    --variable=date:2025-05-01\
+    --standalone --to=man packages/hnefatafl-ai.1.dj --output=packages/hnefatafl-ai.1
+
+gzip --no-name --best packages/hnefatafl-ai.1
+
 pandoc\
     --variable=title:hnefatafl-client\
     --variable=section:1\
@@ -23,6 +32,7 @@ PACKAGE=$(cargo deb)
 echo $PACKAGE
 lintian $PACKAGE
 
+rm packages/hnefatafl-ai.1.gz
 rm packages/hnefatafl-client.1.gz
 rm packages/hnefatafl-server-full.1.gz
 rm packages/README.txt
