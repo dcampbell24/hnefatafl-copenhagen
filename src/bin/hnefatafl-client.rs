3c4d8830c514984a668cd203d9733b6b1972c6db
David Campbell <dcampbell24@gmail.com>
2025-05-03 17:02:12 -0400

    Don't try to shutdown TCP on redox.

--- src/bin/hnefatafl-client.rs
+++ src/bin/hnefatafl-client.rs
@@ -1655,6 +1655,7 @@ fn pass_messages() -> impl Stream<Item = Message> {
                         || message_trim == "logout"
                         || message_trim == "quit"
                     {
+                        #[cfg(not(target_os = "redox"))]
                         tcp_stream
                             .shutdown(Shutdown::Both)
                             .expect("shutdown call failed");
