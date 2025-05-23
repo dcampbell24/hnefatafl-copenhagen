39d57e5ee57a10d2a9c8d9f30369e585cb0b8b1b
David Campbell <dcampbell24@gmail.com>
2025-05-23 16:35:58 -0400

    Add IndexNow to the website.

---
+++ src/bin/send-website-urls.rs
@@ -0,0 +1,37 @@
+#[cfg(feature = "urls")]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "http://api.indexnow.org";
+    let json_data = r#"
+{
+    "host": "www.hnefatafl.org",
+    "key": "514969c804234582abafaae69c947790",
+    "keyLocation": "https://hnefatafl.org/514969c804234582abafaae69c947790.txt",
+    "urlList": [
+        "https://hnefatafl.org",
+        "https://hnefatafl.org/index.html",
+        "https://hnefatafl.org/install.html",
+        "https://hnefatafl.org/rules.html"
+    ]
+}
+"#;
+
+    let client = reqwest::blocking::Client::new();
+
+    let response = client
+        .post(url)
+        .header("Content-Type", "application/json; charset=utf-8")
+        .body(json_data.to_string())
+        .send()?;
+
+    println!("Status: {:?}", response);
+
+    let response_body = response.text()?;
+    println!("Response body:\n{}", response_body);
+
+    Ok(())
+}
+
+#[cfg(not(feature = "urls"))]
+fn main() {
+    println!("You didn't enable urls.");
+}
