d2a0ef3cac2ff562603104b1173f4dc1ae965e25
David Campbell <dcampbell24@gmail.com>
2025-06-13 18:18:15 -0400

    Remove the feature www.

--- packages/chocolatey/nsis/build.ps1
+++ packages/chocolatey/nsis/build.ps1
@@ -6,5 +6,5 @@
 # https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe
 # Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass
 
-cargo build --release --example hnefatafl-client --no-default-features --features www
+cargo build --release --example hnefatafl-client --no-default-features
 & 'C:\Program Files (x86)\NSIS\Bin\makensis.exe' .\hnefatafl-client-installer.nsi
